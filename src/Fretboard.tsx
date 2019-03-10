8fb683461e30bffa890cab2c9bd48c190801025d
Christopher Pappas <damassi.pappas@gmail.com>
2019-03-09 23:48:48 -0800

    Add notes

--- src/Fretboard.tsx
+++ src/Fretboard.tsx
@@ -1,33 +1,30 @@
 import React from "react"
 import { Box, Image, Flex } from "rebass"
 import styled from "styled-components"
-import { fill } from "lodash"
+import { Sans, Display } from "./Typography"
 
-const Dots = () => {
-  return (
-    <DotsContainer width="100%" pl={90}>
-      {fill(Array(6), "").map((y, yIndex) => {
-        return (
-          <Flex width="100%" key={yIndex}>
-            {fill(Array(12), "").map((x, xIndex) => {
-              const BASE_DIST = 100
-              const RATIO = 0.64
-              const mr = BASE_DIST - (BASE_DIST / 12) * (xIndex * RATIO)
-
-              return (
-                <Box key={xIndex} mr={mr} mb={14}>
-                  <Dot />
-                </Box>
-              )
-            })}
-          </Flex>
-        )
-      })}
-    </DotsContainer>
-  )
+const notes = {
+  flats: [
+    ["E", "F", "Gb", "G", "Ab", "A", "Bb", "B", "C", "Db", "D", "Eb", "E"],
+    ["B", "C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B"],
+    ["G", "Ab", "A", "Bb", "B", "C", "Db", "D", "Eb", "E", "F", "Gb", "G"],
+    ["D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B", "C", "Db", "D"],
+    ["A", "Bb", "B", "C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A"],
+    ["E", "F", "Gb", "G", "Ab", "A", "Bb", "B", "C", "Db", "D", "Eb", "E"],
+  ],
+  sharps: [
+    ["E", "F", "F#", "G", "G#", "A", "A#", "B", "C", "C#", "D", "D#", "E"],
+    ["B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"],
+    ["G", "G#", "A", "B#", "B", "C", "C#", "D", "D#", "E", "F", "G#", "G"],
+    ["D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B", "C", "C#", "D"],
+    ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A"],
+    ["E", "F", "F#", "G", "G#", "A", "A#", "B", "C", "C#", "D", "D#", "E"],
+  ],
 }
 
-export const Fretboard = () => {
+export const Fretboard = props => {
+  const guitar = notes[props.accidentalMode || "flats"]
+
   return (
     <Container flexDirection="column" justifyContent="center">
       <Image
@@ -35,7 +32,33 @@ export const Fretboard = () => {
         height={220}
         src={require("./assets/fretboard.png")}
       />
-      <Dots />
+
+      <DotsContainer width="100%" ml={-25}>
+        {guitar.map((str, strIndex) => {
+          return (
+            <Flex width="100%" key={strIndex}>
+              {str.map((note, noteIndex) => {
+                const BASE = 103
+                const DISTANCE_RATIO = 0.6
+                const SPACE = BASE - (BASE / 12) * (noteIndex * DISTANCE_RATIO)
+
+                return (
+                  <Flex
+                    key={noteIndex}
+                    mr={SPACE}
+                    alignItems="top"
+                    justifyContent="center"
+                    style={{ position: "relative" }}
+                  >
+                    <Dot />
+                    <Note size="2">{note}</Note>
+                  </Flex>
+                )
+              })}
+            </Flex>
+          )
+        })}
+      </DotsContainer>
     </Container>
   )
 }
@@ -56,5 +79,12 @@ const Dot = styled(Box)`
   border-radius: 50%;
   width: 20px;
   height: 20px;
+  margin-bottom: 14px;
   background-color: white;
+`
+
+const Note = styled(Display)`
+  position: absolute;
+  color: black;
+  top: 2px;
 `

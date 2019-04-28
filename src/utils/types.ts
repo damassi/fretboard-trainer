3460dc35aad1ce50f5378000ab92a9b38bab7bdd
Christopher Pappas <damassi.pappas@gmail.com>
2019-04-28 00:30:25 -0700

    Simplify interval namings

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -43,18 +43,24 @@ export type IntervalLabels =
   | "unison"
   | "minor 2nd"
   | "♭2"
+  | "m2"
   | "major 2nd"
+  | "M2"
   | "2"
   | "minor 3rd"
   | "♭3"
+  | "m3"
   | "major 3rd"
+  | "M3"
   | "3"
   | "perfect 4th"
   | "4"
   | "#4/♭5"
   | "♭5"
   | "dim 5th"
+  | "d5"
   | "aug 4th"
+  | "a5"
   | "♭4"
   | "#5"
   | "perfect 5th"
@@ -62,12 +68,17 @@ export type IntervalLabels =
   | "#5/♭6"
   | "minor 6th"
   | "♭6"
+  | "m6"
   | "aug 5th"
+  | "a5"
   | "major 6th"
+  | "M6"
   | "6"
   | "minor 7th"
+  | "m7"
   | "♭7"
   | "major 7th"
+  | "M7"
   | "7"
   | "octave"
 
@@ -87,17 +98,17 @@ export type NoteRange = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 1
  */
 export const intervalList: Partial<IntervalLabels>[][] = [
   ["1"],
-  ["minor 2nd", "♭2"],
-  ["major 2nd", "2"],
-  ["minor 3rd", "♭3"],
-  ["major 3rd", "3"],
+  ["minor 2nd", "m2"],
+  ["major 2nd", "M2"],
+  ["minor 3rd", "m3"],
+  ["major 3rd", "M3"],
   ["perfect 4th", "4"],
-  ["#4/♭5", "dim 5th", "aug 4th", "♭5"],
+  ["#4/♭5", "dim 5th", "aug 4th", "d5"],
   ["perfect 5th", "5"],
-  ["#5/♭6", "minor 6th", "aug 5th", "♭6"],
-  ["major 6th", "6"],
-  ["minor 7th", "♭7"],
-  ["major 7th", "7"],
+  ["#5/♭6", "minor 6th", "aug 5th", "m6"],
+  ["major 6th", "M6"],
+  ["minor 7th", "m7"],
+  ["major 7th", "M7"],
   ["1"],
 ]
 
